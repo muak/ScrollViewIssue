69ee4e0ed3e473cc7e49d4459e1829eeec5c13e5
Satoshi Nakamura <kamuxxx@hotmail.com>
2023-08-01 15:17:57 +0900

    First commit

---
+++ ScrollViewIssue/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ScrollViewIssue;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
+
---
+++ ScrollViewIssue/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ScrollViewIssue;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
+
